38a5e00a8c3154405246ba338c5ad7170834c045
kf31313 <kf31313@gmail.com>
2016-06-25 01:49:40 +0800

    Update script.js

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ module.exports = new Script({
                 }
 
                 if (!_.has(scriptRules, upperText)) {
-                    return bot.say(`I didn't understand that.`).then(() => 'speak');
+                    return bot.say(`我不懂您的意思！請您說清楚一點。`).then(() => 'speak');
                 }
 
                 var response = scriptRules[upperText];
